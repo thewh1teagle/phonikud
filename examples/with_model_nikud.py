493b5119ae4761b1894e2fa49ca64aa04d506681
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-02-09 03:05:29 +0200

    Add ONNX model support and update README with example usage

---
+++ examples/with_model_nikud.py
@@ -0,0 +1,24 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "mishkal-ivrit",
+#     "nakdimon-onnx",
+# ]
+#
+# [tool.uv.sources]
+# mishkal-ivrit = { path = "../" }
+# ///
+"""
+wget https://github.com/thewh1teagle/nakdimon-onnx/releases/download/v0.1.0/nakdimon.onnx
+uv run examples/with_model_nikud.py
+"""
+
+from nakdimon_onnx import Nakdimon
+from mishkal import text_to_ipa
+
+nakdimon = Nakdimon("nakdimon.onnx")
+text = "שלום עולם!"
+dotted_text = nakdimon.compute(text)
+
+print('Undotted: ', text_to_ipa(text))
+print('Dotted: ', text_to_ipa(dotted_text))
