ad3d9d89fbcfb0a05f96290bd7825a978c75fd3a
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-02-24 22:29:16 +0200

    Add logging functionality: implement logger for error handling in text expansion

--- mishkal/expander/__init__.py
+++ mishkal/expander/__init__.py
@@ -7,6 +7,7 @@ from .numbers import num_to_word
 from .dates import date_to_word
 from .time_to_word import time_to_word
 from .dictionary import Dictionary
+from mishkal.log import log
 
 class Expander:
     def __init__(self):
@@ -16,10 +17,14 @@ class Expander:
         text = self.dictionary.expand_text(text)
         words = []
         for source_word in text.split():
-            word = date_to_word(source_word)
-            if word == source_word:
-                word = time_to_word(word)
-            if word == source_word:
-                word = num_to_word(word)
-            words.append(word)
+            try:
+                word = date_to_word(source_word)
+                if word == source_word:
+                    word = time_to_word(word)
+                if word == source_word:
+                    word = num_to_word(word)
+                words.append(word)
+            except Exception as e:
+                log.error(f'Failed to expand {word} with error: {e}')
+                words.append(source_word)
         return ' '.join(words)
