eeda6de61583a43e2c5c53884a766bcba8041f0e
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-02-26 12:31:55 +0200

    Add pytest as a development dependency, update Hebrew phonemization entries, and introduce tests for dictionary validation

---
+++ tests/test_dicts.py
@@ -0,0 +1,11 @@
+from mishkal.expander.dictionary import Dictionary
+from mishkal import vocab
+
+def test_dictionary():
+    phonemes_without_punctuation = vocab.SET_OUTPUT_CHARACTERS.difference(vocab.SET_PUNCTUATION)
+    dict = Dictionary()
+    for k, v in dict.dict.items():
+        for c in v:
+            assert c in phonemes_without_punctuation, f'The value of the word {k} is invalid: {v} the invalid character: {c}'
+        
+        
