1c6060e7f1c613e7f98cbfd97ad98e333b05ab40
Morris Alper <morrisalp@gmail.com>
2025-03-25 16:20:31 +0100

    ruff format

--- tests/test_phonemize.py
+++ tests/test_phonemize.py
@@ -16,8 +16,7 @@ def test_phonemize_hebrew_sanity():
 def test_phonemize_hebrew_manual():
     UNICODE_STRESS_MARK = chr(712)
     filename = os.path.join(
-        os.path.dirname(__file__),
-        "manual_phonemize_test_cases.csv"
+        os.path.dirname(__file__), "manual_phonemize_test_cases.csv"
     )
     df = pd.read_csv(filename)
 
@@ -25,12 +24,13 @@ def test_phonemize_hebrew_manual():
         output = phonemize(nikkud, preserve_stress=using_stress)
         x, y = ipa, output
         if not using_stress:
-            x = x.replace(UNICODE_STRESS_MARK, '')
-            y = y.replace(UNICODE_STRESS_MARK, '')
+            x = x.replace(UNICODE_STRESS_MARK, "")
+            y = y.replace(UNICODE_STRESS_MARK, "")
         parenthetical = "" if using_stress else "; no stress"
         if x != y:
             warnings.warn(
-                f"Incorrect phonemization: {x} != {y} ({nikkud}{parenthetical})")
+                f"Incorrect phonemization: {x} != {y} ({nikkud}{parenthetical})"
+            )
             # ^TODO: use assert once all tests pass
 
     def check_pair(nikkud, ipa):
@@ -43,11 +43,10 @@ def test_phonemize_hebrew_manual():
         check_output(nikkud, ipa, False)
 
     for row in df.itertuples():
-
         nikkud_to_check = [row.hebrew_with_nikkud]
         # also test punctuation variants:
         if '"' in row.hebrew_with_nikkud:
-            nikkud_to_check.append(row.hebrew_with_nikkud.replace('"', '״'))
+            nikkud_to_check.append(row.hebrew_with_nikkud.replace('"', "״"))
         if "'" in row.hebrew_with_nikkud:
             nikkud_to_check.append(row.hebrew_with_nikkud.replace("'", "׳"))
         for nikkud in nikkud_to_check:
