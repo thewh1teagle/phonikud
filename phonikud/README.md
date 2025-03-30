5aed31c492b9f14af404731d92f0cdbe339c4d1f
Morris Alper <morrisalp@gmail.com>
2025-03-30 18:35:54 +0300

    partially implement training; need to debug sequence lengths

--- phonikud/README.md
+++ phonikud/README.md
@@ -7,6 +7,7 @@ Test model: `python src/test.py`
 TODO:
 * Add documentation
 * Refactoring:
-  * Make PhonikudModel class extending BertForDiacritization
+  * Make PhonikudModel class extending BertForDiacritization, override predict method to include new outputs rather than current monkey-patch, extend MenakedLogitsOutput -- based on [this code](https://huggingface.co/dicta-il/dictabert-large-char-menaked/blob/main/BertForDiacritization.py)
   * Organize train/val/test splits -- track val performance over time, log to tensorboard/wandb, ...
 * Clean up flags in scripts
+* Check that stress/shva targets are guaranteed to be aligned with tokenized characters
--- phonikud/src/train.py
+++ phonikud/src/train.py
@@ -5,6 +5,8 @@ import os
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm, trange
 import torch
+from torch import nn
+from torch.nn.utils.rnn import pad_sequence
 
 
 def get_opts():
@@ -30,18 +32,23 @@ class AnnotatedLine:
 
     def __init__(self, raw_text):
         self.text = "" # will contain plain hebrew text
-        self.stress = [] # will contain 0/1 for each character (1=stressed)
-        self.mobile_shva = [] # will contain 0/1 for each caracter (1=mobile shva)
+        stress = [] # will contain 0/1 for each character (1=stressed)
+        mobile_shva = [] # will contain 0/1 for each caracter (1=mobile shva)
         for char in raw_text:
             if char == self.STRESS_CHAR:
-                self.stress[-1] = 1
+                stress[-1] = 1
             elif char == self.MOBILE_SHVA_CHAR:
-                self.mobile_shva[-1] = 1
+                mobile_shva[-1] = 1
             else:
                 self.text += char
-                self.stress += [0]
-                self.mobile_shva += [0]
-        assert len(self.text) == len(self.stress) == len(self.mobile_shva)
+                stress += [0]
+                mobile_shva += [0]
+        assert len(self.text) == len(stress) == len(mobile_shva)
+        stress_tensor = torch.tensor(stress)
+        mobile_shva_tensor = torch.tensor(mobile_shva)
+
+        self.target = torch.stack((stress_tensor, mobile_shva_tensor))
+        # ^ shape: (n_chars, 2)
 
 class TrainData(Dataset):
 
@@ -66,35 +73,98 @@ class TrainData(Dataset):
         return AnnotatedLine(text)
 
 
+class PhoNikudModel(nn.Module):
+    # TODO: make it a Transformers module, instead of wrapping existing module
+    def __init__(self, args):
+        super().__init__()
+        self.device = args.device
+        self.base_model = AutoModel.from_pretrained(
+            args.model_checkpoint, trust_remote_code=True)
+
+        self.mlp = nn.Sequential(
+            nn.Linear(1024, 100),
+            nn.ReLU(),
+            nn.Linear(100, 2)
+        )
+        # ^ predicts stress and mobile shva; outputs are logits
+
+    def freeze_base_model(self):
+        self.base_model.eval()
+        for param in self.base_model.parameters():
+            param.requires_grad = False
+    
+    def forward(self, x):
+        # based on: https://huggingface.co/dicta-il/dictabert-large-char-menaked/blob/main/BertForDiacritization.py
+        bert_outputs = self.base_model.bert(**x)
+        hidden_states = bert_outputs.last_hidden_state
+        # ^ shape: (batch_size, n_chars_padded, 1024)
+        hidden_states = self.base_model.dropout(hidden_states)
+
+        _, nikkud_logits = self.base_model.menaked(hidden_states)
+        # ^ nikkud_logits: MenakedLogitsOutput
+
+        additional_logits = self.mlp(hidden_states)
+        # ^ shape: (batch_size, n_chars_padded, 2) [2 for stress & mobile shva]
+
+        return nikkud_logits, additional_logits
+
+
+class Collator:
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+
+    def collate_fn(self, items):
+        inputs = self.tokenizer(
+            [x.text for x in items],
+            padding=True,
+            truncation=True,
+            return_tensors='pt')
+        targets = pad_sequence([x.target.T for x in items], batch_first=True)
+        # ^ shape: (batch_size, n_chars_padded, 2)
+
+        return inputs, targets
+
+
 def main():
     args = get_opts()
 
     print("Loading model...")
 
-    tokenizer = AutoTokenizer.from_pretrained(args.model_checkpoint)
-    model = AutoModel.from_pretrained(
-        args.model_checkpoint, trust_remote_code=True)
+    model = PhoNikudModel(args)
     model.to(args.device)
+    model.freeze_base_model()
+    # ^ we will only train extra layers
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model_checkpoint)
+    collator = Collator(tokenizer)
 
     print("Loading data...")
     data = TrainData(args)
-
-    import pdb; pdb.set_trace()
 
     print("Training...")
 
     dl = DataLoader(data,
         batch_size=args.batch_size,
         shuffle=True,
-        collate_fn=lambda x: x,
+        collate_fn=collator.collate_fn,
         num_workers=args.num_workers
     )
 
     optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate)
+    criterion = nn.BCEWithLogitsLoss()
 
     for _ in trange(args.epochs, desc="Epoch"):
-        for B in tqdm(dl, desc="Train iteration"):
-            ...
+        for inputs, targets in tqdm(dl, desc="Train iteration"):
+            inputs = inputs.to(args.device)
+            targets = targets.to(args.device)
+            # ^ shape: (batch_size, n_chars_padded, 2)
+            _, additional_logits = model(inputs)
+            # ^ shape: (batch_size, n_chars_padded, 2)
+
+            loss = criterion(additional_logits, targets)
+            # ^ NOTE: loss is only on new labels (stress, mobile shva)
+            # rest of network is frozen so nikkud predictions should not change
+            import pdb; pdb.set_trace()
     
     save_dir = args.output_dir
     print("Saving trained model to:", save_dir)
