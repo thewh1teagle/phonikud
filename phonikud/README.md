86b68868ac444d72d4447189f9907b9d6dca94f3
Morris Alper <morrisalp@gmail.com>
2025-03-30 19:03:52 +0300

    full implem

--- phonikud/README.md
+++ phonikud/README.md
@@ -6,8 +6,12 @@ Test model: `python src/test.py`
 
 TODO:
 * Add documentation
+* Implement:
+  * save trained model checkpoint (extend BertForDiacritization, use transformers save_model())
+  * test.py
 * Refactoring:
   * Make PhonikudModel class extending BertForDiacritization, override predict method to include new outputs rather than current monkey-patch, extend MenakedLogitsOutput -- based on [this code](https://huggingface.co/dicta-il/dictabert-large-char-menaked/blob/main/BertForDiacritization.py)
   * Organize train/val/test splits -- track val performance over time, log to tensorboard/wandb, ...
+  * Move utils like `is_hebrew_letter`, stress character value etc. into a separate script (`hebrew_utils.py`?)
 * Clean up flags in scripts
-* Check that stress/shva targets are guaranteed to be aligned with tokenized characters
+* Check that stress/shva targets are guaranteed to be aligned with tokenized characters (use `return_offsets_mapping=True`? cf. dictabert code)
--- phonikud/src/train.py
+++ phonikud/src/train.py
@@ -8,6 +8,23 @@ import torch
 from torch import nn
 from torch.nn.utils.rnn import pad_sequence
 
+ALEF_ORD = ord('א')
+TAF_ORD = ord('ת')
+def is_hebrew_letter(char):
+   return ALEF_ORD <= ord(char) <= TAF_ORD
+
+MATRES_LETTERS = list('אוי')
+def is_matres_letter(char):
+    return char in MATRES_LETTERS
+
+import re
+nikud_pattern = re.compile(r'[\u05B0-\u05BD\u05C1\u05C2\u05C7]')
+def remove_nikkud(text):
+    return nikud_pattern.sub('', text)
+
+STRESS_CHAR = chr(1451) # "ole" symbol marks stress
+MOBILE_SHVA_CHAR = chr(1469) # "meteg" symbol marks shva na (mobile shva)
+
 
 def get_opts():
     parser = ArgumentParser()
@@ -27,17 +44,15 @@ def get_opts():
     return parser.parse_args()
 
 class AnnotatedLine:
-    STRESS_CHAR = chr(1451) # "ole" symbol marks stress
-    MOBILE_SHVA_CHAR = chr(1469) # "meteg" symbol marks shva na (mobile shva)
-
+    
     def __init__(self, raw_text):
         self.text = "" # will contain plain hebrew text
         stress = [] # will contain 0/1 for each character (1=stressed)
         mobile_shva = [] # will contain 0/1 for each caracter (1=mobile shva)
         for char in raw_text:
-            if char == self.STRESS_CHAR:
+            if char == STRESS_CHAR:
                 stress[-1] = 1
-            elif char == self.MOBILE_SHVA_CHAR:
+            elif char == MOBILE_SHVA_CHAR:
                 mobile_shva[-1] = 1
             else:
                 self.text += char
@@ -88,6 +103,8 @@ class PhoNikudModel(nn.Module):
         )
         # ^ predicts stress and mobile shva; outputs are logits
 
+        self.base_config = self.base_model.config
+
     def freeze_base_model(self):
         self.base_model.eval()
         for param in self.base_model.parameters():
@@ -100,13 +117,70 @@ class PhoNikudModel(nn.Module):
         # ^ shape: (batch_size, n_chars_padded, 1024)
         hidden_states = self.base_model.dropout(hidden_states)
 
-        _, nikkud_logits = self.base_model.menaked(hidden_states)
-        # ^ nikkud_logits: MenakedLogitsOutput
+        _, nikud_logits = self.base_model.menaked(hidden_states)
+        # ^ nikud_logits: MenakedLogitsOutput
 
         additional_logits = self.mlp(hidden_states)
         # ^ shape: (batch_size, n_chars_padded, 2) [2 for stress & mobile shva]
 
-        return nikkud_logits, additional_logits
+        return nikud_logits, additional_logits
+
+    @torch.no_grad()
+    def predict(self, sentences, tokenizer, mark_matres_lectionis=None, padding='longest'):
+        # based on: https://huggingface.co/dicta-il/dictabert-large-char-menaked/blob/main/BertForDiacritization.py
+
+        sentences = [remove_nikkud(sentence) for sentence in sentences]
+        # assert the lengths aren't out of range
+        assert all(len(sentence) + 2 <= tokenizer.model_max_length for sentence in sentences), f'All sentences must be <= {tokenizer.model_max_length}, please segment and try again'
+        
+        # tokenize the inputs and convert them to relevant device
+        inputs = tokenizer(sentences, padding=padding, truncation=True, return_tensors='pt', return_offsets_mapping=True)
+        offset_mapping = inputs.pop('offset_mapping')
+        inputs = {k:v.to(self.device) for k,v in inputs.items()}
+        
+        # calculate the predictions
+        nikud_logits, additional_logits = self.forward(inputs)
+        nikud_predictions = nikud_logits.nikud_logits.argmax(dim=-1).tolist()
+        shin_predictions = nikud_logits.shin_logits.argmax(dim=-1).tolist()
+
+        stress_predictions = (additional_logits[..., 0] > 0).int().tolist()
+        mobile_shva_predictions = (additional_logits[..., 1] > 0).int().tolist()
+
+        ret = []
+        for sent_idx,(sentence,sent_offsets) in enumerate(zip(sentences, offset_mapping)):
+            # assign the nikud to each letter!
+            output = []
+            prev_index = 0
+            for idx,offsets in enumerate(sent_offsets):
+                # add in anything we missed
+                if offsets[0] > prev_index:
+                    output.append(sentence[prev_index:offsets[0]])
+                if offsets[1] - offsets[0] != 1: continue
+                
+                # get our next char
+                char = sentence[offsets[0]:offsets[1]]
+                prev_index = offsets[1]
+                if not is_hebrew_letter(char):
+                    output.append(char)
+                    continue
+                
+                nikud = self.base_config.nikud_classes[nikud_predictions[sent_idx][idx]]
+                shin = '' if char != 'ש' else self.base_config.shin_classes[shin_predictions[sent_idx][idx]] 
+
+                # check for matres lectionis
+                if nikud == self.base_config.mat_lect_token:
+                    if not is_matres_letter(char): nikud = '' # don't allow matres on irrelevant letters
+                    elif mark_matres_lectionis is not None: nikud = mark_matres_lectionis
+                    else: continue
+                
+                stress = STRESS_CHAR if stress_predictions[sent_idx][idx] == 1 else ""
+                mobile_shva = MOBILE_SHVA_CHAR if mobile_shva_predictions[sent_idx][idx] == 1 else ""
+
+                output.append(char + shin + nikud + stress + mobile_shva)
+            output.append(sentence[prev_index:])
+            ret.append(''.join(output))
+        
+        return ret
 
 
 class Collator:
@@ -154,21 +228,32 @@ def main():
     criterion = nn.BCEWithLogitsLoss()
 
     for _ in trange(args.epochs, desc="Epoch"):
-        for inputs, targets in tqdm(dl, desc="Train iteration"):
+        pbar = tqdm(dl, desc="Train iter")
+        for inputs, targets in pbar:
+
+            optimizer.zero_grad()
+
             inputs = inputs.to(args.device)
             targets = targets.to(args.device)
             # ^ shape: (batch_size, n_chars_padded, 2)
             _, additional_logits = model(inputs)
             # ^ shape: (batch_size, n_chars_padded, 2)
 
-            loss = criterion(additional_logits, targets)
+            loss = criterion(
+                additional_logits[:, 1:-1], # skip BOS and EOS symbols
+                targets.float())
             # ^ NOTE: loss is only on new labels (stress, mobile shva)
             # rest of network is frozen so nikkud predictions should not change
-            import pdb; pdb.set_trace()
+
+            loss.backward()
+            optimizer.step()
+
+            pbar.set_description(f"Train iter (L={loss.item():.4f})")
     
-    save_dir = args.output_dir
-    print("Saving trained model to:", save_dir)
-    model.save_model(save_dir)
+    # TODO: save checkpoint
+    # save_dir = args.output_dir
+    # print("Saving trained model to:", save_dir)
+    # model.save_model(save_dir)
 
     print("Testing...")
 
@@ -179,17 +264,9 @@ def main():
         test_text = f.read().strip()
 
     for line in test_text.splitlines():
-        ...
-
-    # import pdb; pdb.set_trace()
-
-    # print("Testing...")
-
-    # model.eval()
-    # sentence = 'בשנת 1948 השלים אפרים קישון את לימודיו בפיסול מתכת ובתולדות האמנות והחל לפרסם מאמרים הומוריסטיים'
-    # print(model.predict([sentence], tokenizer))
-
-    # print("Done")
+        print(line)
+        print(model.predict([line], tokenizer, mark_matres_lectionis='*'))
+        print()
 
 
 if __name__ == "__main__":
