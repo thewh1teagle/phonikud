fb991de03492fb0de5c7eb4b6a011096b8569623
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-04-04 01:24:20 +0300

    add dicta onnx example

--- examples/with_dicta.py
+++ examples/with_dicta.py
@@ -1,12 +1,19 @@
+"""
+pip install transformers torch mishkal
+uv run with_dicta.py
+"""
+
 from transformers import AutoModel, AutoTokenizer
 from mishkal import phonemize
 
-tokenizer = AutoTokenizer.from_pretrained('dicta-il/dictabert-large-char-menaked')
-model = AutoModel.from_pretrained('dicta-il/dictabert-large-char-menaked', trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained("dicta-il/dictabert-large-char-menaked")
+model = AutoModel.from_pretrained(
+    "dicta-il/dictabert-large-char-menaked", trust_remote_code=True
+)
 
 model.eval()
 
-sentence = 'בשנת 1948 השלים אפרים קישון את לימודיו בפיסול מתכת ובתולדות האמנות והחל לפרסם מאמרים הומוריסטיים'
+sentence = "בשנת 1948 השלים אפרים קישון את לימודיו בפיסול מתכת ובתולדות האמנות והחל לפרסם מאמרים הומוריסטיים"
 niqqud = model.predict([sentence], tokenizer)
 phonemes = phonemize(niqqud)
 print(niqqud)
