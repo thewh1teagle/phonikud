fac78891475d805dfc7f37937c04a03c69759ae5
Morris Alper <morrisalp@gmail.com>
2025-04-05 14:45:32 -0400

    split phonemize tests into basic (will fail with error) and advanced (will display warning)

--- tests/test_phonemize.py
+++ tests/test_phonemize.py
@@ -5,6 +5,8 @@ import warnings
 
 TEST_STRESS = False
 
+UNICODE_STRESS_MARK = chr(712)
+
 
 def test_phonemize_hebrew_sanity():
     with_stress = phonemize("שָׁ֫לוֹם", preserve_stress=True)
@@ -13,41 +15,49 @@ def test_phonemize_hebrew_sanity():
 
 
 def test_phonemize_hebrew_manual():
-    UNICODE_STRESS_MARK = chr(712)
-    filename = str(Path(__file__).parent / "manual_phonemize_test_cases.csv")
-    df = pd.read_csv(filename)
 
-    def check_output(nikkud, ipa, using_stress):
-        output = phonemize(nikkud, preserve_stress=using_stress)
-        x, y = ipa, output
-        if not using_stress:
-            x = x.replace(UNICODE_STRESS_MARK, "")
-            y = y.replace(UNICODE_STRESS_MARK, "")
-        parenthetical = "" if using_stress else "; no stress"
-        if x != y:
-            warnings.warn(
-                f"Incorrect phonemization: {x} != {y} ({nikkud}{parenthetical})"
-            )
-            # ^TODO: use assert once all tests pass
+    def test_file(basename, warn_only=False):
+        filename = str(Path(__file__).parent / basename)
+        df = pd.read_csv(filename)
 
-    def check_pair(nikkud, ipa):
-        has_stress = UNICODE_STRESS_MARK in row.ipa
-        # ^ if stress is marked manually, check both with and without stress
-        # otherwise just check that output is correct disregarding stress
+        def check_output(nikkud, ipa, using_stress):
+            output = phonemize(nikkud, preserve_stress=using_stress)
+            x, y = ipa, output
+            if not using_stress:
+                x = x.replace(UNICODE_STRESS_MARK, "")
+                y = y.replace(UNICODE_STRESS_MARK, "")
+            parenthetical = "" if using_stress else "; no stress"
+            if warn_only:
+                if x != y:
+                    warnings.warn(
+                        f"Incorrect phonemization: {x} != {y} ({nikkud}{parenthetical})"
+                    )
+            else:
+                assert x == y, f"Incorrect phonemization: {x} != {y} ({nikkud}{parenthetical})"
 
-        if TEST_STRESS and has_stress:
-            check_output(nikkud, ipa, True)
-        check_output(nikkud, ipa, False)
+        def check_pair(nikkud, ipa):
+            has_stress = UNICODE_STRESS_MARK in row.ipa
+            # ^ if stress is marked manually, check both with and without stress
+            # otherwise just check that output is correct disregarding stress
 
-    for row in df.itertuples():
-        nikkud_to_check = [row.hebrew_with_nikkud]
-        # also test punctuation variants:
-        if '"' in row.hebrew_with_nikkud:
-            nikkud_to_check.append(row.hebrew_with_nikkud.replace('"', "״"))
-        if "'" in row.hebrew_with_nikkud:
-            nikkud_to_check.append(row.hebrew_with_nikkud.replace("'", "׳"))
-        for nikkud in nikkud_to_check:
-            check_pair(nikkud, row.ipa)
+            if TEST_STRESS and has_stress:
+                check_output(nikkud, ipa, True)
+            check_output(nikkud, ipa, False)
+
+        for row in df.itertuples():
+            nikkud_to_check = [row.hebrew_with_nikkud]
+            # also test punctuation variants:
+            if '"' in row.hebrew_with_nikkud:
+                nikkud_to_check.append(
+                    row.hebrew_with_nikkud.replace('"', "״"))
+            if "'" in row.hebrew_with_nikkud:
+                nikkud_to_check.append(
+                    row.hebrew_with_nikkud.replace("'", "׳"))
+            for nikkud in nikkud_to_check:
+                check_pair(nikkud, row.ipa)
+
+    test_file("phonemize_test_tables/basic.csv")
+    test_file("phonemize_test_tables/advanced.csv", warn_only=True)
 
 
 def test_phonemize_hebrew_sentences():
