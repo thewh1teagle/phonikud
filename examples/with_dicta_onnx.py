acaedda02b09e0b86d061912edb305eae7b6537a
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-04-10 23:48:11 +0300

    feat: add new sentence to phonemization examples and update ONNX model path

--- examples/with_dicta_onnx.py
+++ examples/with_dicta_onnx.py
@@ -1,15 +1,15 @@
 """
 pip install -U dicta-onnx mishkal
 
-wget https://github.com/thewh1teagle/dicta-onnx/releases/download/model-files-v1.0/dicta-1.0.onnx
-uv run with_dicta_onnx.py
+wget https://github.com/thewh1teagle/dicta-onnx/releases/download/model-files-v1.0/dicta-1.0.int8.onnx
+uv run examples/with_dicta_onnx.py
 """
 
 from dicta_onnx import Dicta
 from mishkal import phonemize
 
-dicta = Dicta("./dicta-1.0.onnx")
-sentence = "בשנת 1948 השלים אפרים קישון את לימודיו בפיסול מתכת ובתולדות האמנות והחל לפרסם מאמרים הומוריסטיים"
+dicta = Dicta("./dicta-1.0.int8.onnx")
+sentence = "בשנת 1948 השלים אפרים קישון את לימודיו בפיסול מתכת ובתולדות האמנות והחל לפרסם מאמרים הומוריסטיים על כל מה שרצה"
 with_diacritics = dicta.add_diacritics(sentence)
 phonemes = phonemize(with_diacritics)
 print("Sentence: ", with_diacritics)
