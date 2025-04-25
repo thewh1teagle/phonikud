0ba196a1be47771027461e791808d2ee2489776f
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-04-25 20:24:37 +0300

    Refactor Hebrew model: remove hebrew.py, add base_model.py, and update train/test scripts for improved structure and functionality

--- phonikud/src/test.py
+++ phonikud/src/test.py
@@ -1,9 +1,36 @@
 """
-uv run src/test.py
+uv run src/test.py --device cpu --file data/test.txt
 """
 
+from model import PhoNikudModel
+from argparse import ArgumentParser
+from transformers import AutoTokenizer
+
+def get_opts():
+    parser = ArgumentParser()
+    parser.add_argument('-m', '--model_checkpoint',
+                        default='./output/phonikud_ckpt', type=str)
+    parser.add_argument('-d', '--device',
+                        default='cuda', type=str)
+    # test file path
+    parser.add_argument('-t', '--file',
+                        default='./data/test.txt', type=str)
+    return parser
+
 def main():
-    ...
+    parser = get_opts()
+    args = parser.parse_args()
+    model = PhoNikudModel.from_pretrained(args.model_checkpoint, trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_checkpoint)
+    model.to(args.device)
+    model.eval()
+
+    with open(args.file, 'r', encoding='utf-8') as fp:
+        for line in fp:
+            line = line.strip()
+            if not line:
+                continue
+            print(model.predict([line], tokenizer, mark_matres_lectionis='*'))
 
 
 if __name__ == "__main__":
