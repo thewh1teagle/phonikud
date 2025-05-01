f3f755fee1a3cbf707553328d9d1ea60ca56e52c
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-05-01 20:51:24 +0300

    enhance editor

--- examples/editor.py
+++ examples/editor.py
@@ -8,39 +8,31 @@ from mishkal import phonemize, normalize
 import gradio as gr
 
 default_text = """
-כָּל עֶ֫רֶב יָאִ֫יר (הַשֵּׁ֫ם הַמָּלֵ֫א וּמֽק֫וֹם הָעֲבוֹדָ֫ה שֶׁלּ֫וֹ שְׁמוּרִ֫ים בַּמַּעֲרֶ֫כֶת) רָץ 20 קִילוֹמֶ֫טֶר. הוּא מֽסַפֵּ֫ר לִי שֶׁזֶּ֫ה מֽנַקֶּ֫ה לוֹ אֶת הָרֹ֫אשׁ אַחֲרֵ֫י הָעֲבוֹדָ֫ה, "שָׁעָ֫ה וָחֵ֫צִי בְּלִ֫י עֲבוֹדָ֫ה, אִשָּׁ֫ה וִילָדִ֫ים" כְּמ֫וֹ שֶׁה֫וּא מַגְדִּ֫יר זֹאת. אֲבָ֫ל אַחֲרֵ֫י הַמִּקְלַ֫חַת הוּא מַתְחִ֫יל בּֽמָ֫ה שֶׁנִּתָּ֫ן לֽכַנּ֫וֹת הָעֲבוֹדָ֫ה הַשְּׁנִיָּ֫ה שֶׁלּ֫וֹ: לִמְצֹ֫א לוֹ קוֹלֵ֫גוֹת חֲדָשׁ֫וֹת לָעֲבוֹדָ֫ה, כִּי יָאִ֫יר הוּא כַּנִּרְאֶ֫ה הַמֶּ֫לֶךְ שֶׁל "חָבֵ֫ר מֵבִ֫יא חָבֵ֫ר" בּֽיִשְׂרָאֵ֫ל.
-דֻּגְמָא מַגְנִיבָה: [אנציקלופדיה](/ʔantsiklopˈedja/)
-"""
+כׇּ֫ל עֶ֫רֶב יָאִ֫יר (הַשֵּׁ֫ם הַמָּלֵ֫א וּמֽק֫וֹם הָעֲבוֹדָ֫ה שֶׁלּ֫וֹ שְׁמוּרִ֫ים בַּמַּעֲרֶ֫כֶת) רָ֫ץ 20 קִילוֹמֶ֫טֶר. ה֫וּא מֽסַפֵּ֫ר לִ֫י שֶׁזֶּ֫ה מֽנַקֶּ֫ה ל֫וֹ אֶ֫ת הָרֹ֫אשׁ אַחֲרֵ֫י הָעֲבוֹדָ֫ה, "שָׁעָ֫ה וָחֵ֫צִי בְּלִ֫י עֲבוֹדָ֫ה, אִשָּׁ֫ה וִילָדִ֫ים" כְּמ֫וֹ שֶׁה֫וּא מַגְדִּ֫יר זֹ֫את. אֲבָ֫ל אַחֲרֵ֫י הַמִּקְלַ֫חַת ה֫וּא מַתְחִ֫יל בּֽמָ֫ה שֶׁנִּתָּ֫ן לֽכַנּ֫וֹת הָעֲבוֹדָ֫ה הַשְּׁנִיָּ֫ה שֶׁלּ֫וֹ: לִמְצֹ֫א ל֫וֹ קוֹלֵ֫גוֹת חֲדָשׁ֫וֹת לָעֲבוֹדָ֫ה, כִּ֫י יָאִ֫יר ה֫וּא כַּנִּרְאֶ֫ה הַמֶּ֫לֶךְ שֶׁ֫ל "חָבֵ֫ר מֵבִ֫יא חָבֵ֫ר" בּֽיִשְׂרָאֵ֫ל.
+""".strip()
 
 theme = gr.themes.Soft(font=[gr.themes.GoogleFont("Roboto")])
 
 
-def on_submit_debug(text: str, predict_stress) -> str:
-    phonemes = phonemize(
-        text, preserve_punctuation=True, predict_stress=predict_stress, schema="modern"
-    )
-    normalized_text = normalize(text)
-    return phonemes + "\n\nNormalized:\n" + normalized_text
-
-
-def on_submit(text: str, predict_stress) -> str:
-    return phonemize(text, predict_stress=predict_stress)
+def on_submit(text: str, predict_stress, schema: str) -> str:
+    return phonemize(text, predict_stress=predict_stress, schema=schema)
 
 
 with gr.Blocks(theme=theme) as demo:
     text_input = gr.Textbox(
-        value=default_text, label="Text", rtl=True, elem_classes=["input"]
+        value=default_text, label="Text", rtl=True, elem_classes=["input"], lines=5
     )
-    debug_checkbox = gr.Checkbox(value=False, label="Enable Debug Mode")
+
+    schema_dropdown = gr.Dropdown(
+        choices=["modern", "plain"], value="plain", label="Phoneme Schema"
+    )
     predict_stress_checkbox = gr.Checkbox(value=False, label="Predict Stress")
-    phonemes_output = gr.Textbox(label="Phonemes")
+    phonemes_output = gr.Textbox(label="Phonemes", lines=5)
     submit_button = gr.Button("Create")
 
     submit_button.click(
-        fn=lambda text, debug, stress: on_submit_debug(text, stress)
-        if debug
-        else on_submit(text, stress),
-        inputs=[text_input, debug_checkbox, predict_stress_checkbox],
+        fn=on_submit,
+        inputs=[text_input, predict_stress_checkbox, schema_dropdown],
         outputs=[phonemes_output],
     )
     ## center markdown
