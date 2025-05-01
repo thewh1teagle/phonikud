0fbb344b80a240e74c561e321aeb7d6669159e92
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-05-01 19:47:34 +0300

    enhance syllables tests with single csv and two columns

--- tests/test_syllables.py
+++ tests/test_syllables.py
@@ -7,20 +7,19 @@ from mishkal.syllables import add_stress, sort_diacritics
 from pathlib import Path
 
 base_path = Path(__file__).parent / "syllables_test_tables"
-second_filename = base_path / "second.csv"
-third_filename = base_path / "third.csv"
+files = base_path.glob("*.csv")
 
 STRESS = "\u05ab"
 DAGESH = "\u05bc"
 
 
-def check_syllables(filename, stress_index: int):
+def check_syllables(filename):
     df = pd.read_csv(filename)
     for row in df.itertuples():
         src_word = sort_diacritics(row.word)
         src_word = src_word.replace(DAGESH, "")
         src_without_stress = src_word.replace(STRESS, "")
-        dst_with_stress = add_stress(src_without_stress, stress_index)
+        dst_with_stress = add_stress(src_without_stress, int(row.stress_index))
 
         dst_with_stress = sort_diacritics(dst_with_stress)
         assert dst_with_stress == src_word, (
@@ -29,5 +28,5 @@ def check_syllables(filename, stress_index: int):
 
 
 def test_syllables():
-    check_syllables(second_filename, -2)
-    check_syllables(third_filename, -3)
+    for file in files:
+        check_syllables(file)
