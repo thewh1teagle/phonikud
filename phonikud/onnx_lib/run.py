90b8f3f8d7c614f30487f116cfe2beb957e2c706
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-05-03 23:33:51 +0300

    Update model path in main function to use int8 ONNX model

--- phonikud/onnx_lib/run.py
+++ phonikud/onnx_lib/run.py
@@ -47,7 +47,7 @@ class Dicta:
 
 
 def main():
-    dicta = Dicta("./dicta-1.0.onnx")
+    dicta = Dicta("./dicta-1.0.int8.onnx")
     sentence = "בשנת 1948 השלים אפרים קישון את לימודיו בפיסול מתכת ובתולדות האמנות והחל לפרסם מאמרים הומוריסטיים"
     with_diacritics = dicta.add_diacritics(sentence)
     print(with_diacritics)
