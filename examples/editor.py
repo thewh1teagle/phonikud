d59fa239524fb0e0a9b9ff4d361653caa1c63862
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-05-06 12:57:48 +0300

    update editor

--- examples/editor.py
+++ examples/editor.py
@@ -7,15 +7,7 @@ uv run gradio examples/editor.py
 from mishkal import phonemize
 import gradio as gr
 
-default_text = """
-כׇּל עֶ֫רֶב יָאִיר רָץ 20 קִילוֹמֶ֫טֶר.
-הוּא מֽסַפֵּר לִי שֶׁזֶּה מֽנַקֶּה לוֹ אֶת הָרֹאשׁ אַחֲרֵי הָעֲבוֹדָה,
-שָׁעָה וָחֵ֫צִי בְּלִי עֲבוֹדָה, אִשָּׁה וִילָדִים
-כְּמוֹ שֶׁהוּא מַגְדִּיר זֹאת.
-אֲבָל אַחֲרֵי הַמִּקְלַ֫חַת הוּא מַתְחִיל בּֽמָה שֶׁנִּתָּן לֽכַנּוֹת הָעֲבוֹדָה הַשְּׁנִיָּה שֶׁלּוֹ:
-לִמְצֹא לוֹ קוֹלֵ֫גוֹת חֲדָשׁוֹת לָעֲבוֹדָה,
-כִּי יָאִיר הוּא כַּנִּרְאֶה הַמֶּלֶךְ שֶׁל חָבֵר מֵבִיא חָבֵר בּֽיִשְׂרָאֵל.
-""".strip()
+default_text = """כׇּל עֶ֫רֶב יָאִיר רָץ 20 קִילוֹמֶ֫טֶר. הוּא מֽסַפֵּר לִי שֶׁזֶּה מֽנַקֶּה לוֹ אֶת הָרֹאשׁ אַחֲרֵי הָעֲבוֹדָה, שָׁעָה וָחֵ֫צִי בְּלִי עֲבוֹדָה, אִשָּׁה וִילָדִים כְּמוֹ שֶׁהוּא מַגְדִּיר זֹאת. אֲבָל אַחֲרֵי הַמִּקְלַ֫חַת הוּא מַתְחִיל בּֽמָה שֶׁנִּתָּן לֽכַנּוֹת הָעֲבוֹדָה הַשְּׁנִיָּה שֶׁלּוֹ: לִמְצֹא לוֹ קוֹלֵ֫גוֹת חֲדָשׁוֹת לָעֲבוֹדָה, כִּי יָאִיר הוּא כַּנִּרְאֶה הַמֶּלֶךְ שֶׁל חָבֵר מֵבִיא חָבֵר בּֽיִשְׂרָאֵל.""".strip()
 
 theme = gr.themes.Soft(font=[gr.themes.GoogleFont("Roboto")])
 
@@ -34,7 +26,7 @@ with gr.Blocks(theme=theme) as demo:
     schema_dropdown = gr.Dropdown(
         choices=["modern", "plain"], value="plain", label="Phoneme Schema"
     )
-    predict_stress_checkbox = gr.Checkbox(value=False, label="Predict Stress")
+    predict_stress_checkbox = gr.Checkbox(value=True, label="Predict Stress")
     phonemes_output = gr.Textbox(label="Phonemes", lines=5)
     submit_button = gr.Button("Create")
 
