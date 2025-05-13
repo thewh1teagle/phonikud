cfd12dc673a175d3470f8ddb46f0e6d1fa83a982
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-05-13 14:46:39 +0300

    Add initial implementation of diacritics addition in phonikud example

---
+++ phonikud_onnx/examples/file.py
@@ -0,0 +1,24 @@
+"""
+wget https://huggingface.co/thewh1teagle/phonikud/resolve/main/phonikud-1.0.int8.onnx
+uv sync
+uv run python examples/file.py <some file>
+"""
+
+from phonikud_onnx import Phonikud
+from mishkal import lexicon
+import sys
+
+
+def main():
+    phonikud = Phonikud("./phonikud-1.0.int8.onnx")
+    file = sys.argv[1]
+    with open(file) as fp:
+        for line in fp:
+            with_diacritics = phonikud.add_diacritics(
+                line, mark_matres_lectionis=lexicon.NIKUD_HASER_DIACRITIC
+            )
+            print(with_diacritics.strip())
+
+
+if __name__ == "__main__":
+    main()
