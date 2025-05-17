f55fa6799ed1af51051297124bfd7a95f9b74eae
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-05-18 02:48:21 +0300

    update onnx to new max prediction change

--- phonikud_onnx/src/phonikud_onnx/model.py
+++ phonikud_onnx/src/phonikud_onnx/model.py
@@ -130,9 +130,9 @@ class OnnxModel:
         # Get predictions
         nikud_predictions = np.argmax(nikud_logits, axis=-1)
         shin_predictions = np.argmax(shin_logits, axis=-1)
-        stress_predictions = (additional_logits[..., 0] > 0).astype(np.int32)
-        mobile_shva_predictions = (additional_logits[..., 1] > 0).astype(np.int32)
-        prefix_predictions = (additional_logits[..., 2] > 0).astype(np.int32)
+        stress_predictions = (additional_logits[..., 1] > 1).astype(np.int32)
+        mobile_shva_predictions = (additional_logits[..., 2] > 1).astype(np.int32)
+        prefix_predictions = (additional_logits[..., 3] > 1).astype(np.int32)
 
         ret = []
         for sent_idx, (sentence, sent_offsets) in enumerate(
